96845aa582616905183afd3307ad6bb28e881c90
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2020-04-04 11:35:06 +0200

    Initial commit

---
+++ src/main/java/io/github/oliviercailloux/samples/coffee/MyEspressoMachine.java
@@ -0,0 +1,11 @@
+package io.github.oliviercailloux.samples.coffee;
+
+/**
+ * A specific espresso machine, that produces coffee of strength up to 20, whose
+ * power is 2000 watts, and that produces a coffee of strength <em>s</em> in
+ * <em>140 + 2 * s</em> seconds.
+ *
+ */
+public class MyEspressoMachine implements EspressoMachine {
+
+}
